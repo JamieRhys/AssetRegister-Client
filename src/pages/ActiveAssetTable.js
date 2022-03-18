84d52c8b0ea76eac2a71133d6eb416643d50f063
JamieRhys <jamie_airfix@yahoo.co.uk>
2022-03-18 07:06:06 +0000

    Started adding table

--- src/pages/ActiveAssetTable.js
+++ src/pages/ActiveAssetTable.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 
 import { AppBar, Box, IconButton, Menu, Toolbar, Typography } from '@material-ui/core';
-import ArrowBack from '@material-ui/icons/ArrowBack'
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { Link } from 'react-router-dom';
+
+import { ASSET_URL } from './../utils/Constants';
+import { BaseTable } from './../components/Table/BaseTable';
 
 class ActiveAssetTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            assets: []
+        }
+    }
+
+    componentDidMount() {
+        const token = sessionStorage.getItem("jwt");
+
+        fetch(
+            ASSET_URL,
+            {
+                headers: {"Authorization": token}
+            }
+        )
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({ assets: responseData });
+            })
+            .catch(err => console.error(err));
+    }
+
     render() {
+        const tableColumns = [
+            {
+                Header: "Asset Tag",
+                accessor: "assetTag"
+            },
+            {
+                Header: "Asset Type",
+                accessor: "type"
+            },
+            {
+                Header: "Manufacturer",
+                accessor: "manufacturer"
+            },
+            {
+                Header: "Model",
+                accessor: "model"
+            }
+        ];
+
+        const data = this.state.assets;
+
         return(
             <Box sx={{ flexGrow: 1 }}>
                  <AppBar position="static">
@@ -15,8 +65,8 @@ class ActiveAssetTable extends Component {
                             color="inherit"
                             aria-label="menu"
                             sx={{ mr: 2 }}
-                            onClick={this.goBack} >
-                                <ArrowBack />
+                            onClick={ this.onBackButtonClick } >
+                                <MenuIcon />
                         </IconButton>
                         <Menu>
 
@@ -26,6 +76,7 @@ class ActiveAssetTable extends Component {
                         </Typography>
                     </Toolbar>
                 </AppBar>
+                <BaseTable columns={tableColumns} data={this.state.assets} />
             </Box>
         );
     }
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,3 +3,4 @@ const SERVER_URI = "localhost";
 const SERVER_PORT = "8102";
 export const SERVER_URL = SERVER_PREFIX + SERVER_URI + ":" + SERVER_PORT;
 export const USER_URL = SERVER_URL + "/users";
+export const ASSET_URL = SERVER_URL + "/assets";
